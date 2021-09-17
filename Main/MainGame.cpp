72f88b4b3e37397a769d39f2197a78aa42a016ec
ChangMini <dlwlxns4@gmail.com>
2021-09-17 23:38:43 +0900

    feat : 메인

---
+++ Main/MainGame.cpp
@@ -0,0 +1,126 @@
+#include "MainGame.h"
+#include "Singleton.h"
+#include "KeyManager.h"
+#include "SceneManager.h"
+#include "Image.h"
+#include "Iori.h"
+
+void MainGame::Init()
+{
+	//Singleton<int> singleton1;
+	//singleton1.ProcessInputKey();
+	//Singleton<KeyManager>::GetSingleton()->Init();
+
+	//KeyManager keyMgr;
+	KeyManager::GetSingleton()->Init();
+	SceneManager::GetSingleton();
+
+
+	// 타이머 셋팅
+	hTimer = (HANDLE)SetTimer(g_hWnd, 0, 10, NULL);
+
+	mousePosX = 0;
+	mousePosY = 0;
+	//clickedMousePosX = 0; 
+	clickedMousePosY = 0; 
+
+
+	//playerTank.ammoPack[0].SetTarget(&enemyTank);
+	//playerTank.ammo[1].SetTarget(&enemyTank);
+	//playerTank.ammo[2].SetTarget(&enemyTank);
+
+	// 백버퍼
+	backBuffer = new Image;
+	backBuffer->Init(WIN_SIZE_X, WIN_SIZE_Y);
+
+	// 원빈 이미지
+	backGround = new Image;
+	if (!SUCCEEDED(backGround->Init("Image/mapImage.bmp", 1400, 933)))
+	{
+		cout << "Image/bin.bmp 파일 로드에 실패했다." << endl;
+	}
+
+
+
+
+
+
+
+}
+
+void MainGame::Update()
+{
+
+
+
+
+
+	InvalidateRect(g_hWnd, NULL, false);
+}
+
+void MainGame::Render(HDC hdc)
+{
+	HDC hBackBufferDC = backBuffer->GetMemDC();
+
+	wsprintf(text, "MousePosX : %d", mousePosX);
+	TextOut(hBackBufferDC, 200, 10, text, strlen(text));
+
+	wsprintf(text, "MousePosY : %d", mousePosY);
+	TextOut(hBackBufferDC, 200, 40, text, strlen(text));
+
+	backGround->Render(hBackBufferDC);
+
+
+	backBuffer->Render(hdc);
+}
+
+void MainGame::Release()
+{
+	SAFE_RELEASE(backBuffer);
+	SAFE_RELEASE(backGround);
+
+
+	// 타이머 객체 삭제
+	KillTimer(g_hWnd, 0);
+}
+
+LRESULT MainGame::MainProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	switch (iMessage)
+	{
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_SPACE:
+			//playerTank.Fire();
+			break;
+		case VK_UP:
+			//playerTank.Move(MoveDir::Up);
+			break;
+		case VK_DOWN:
+			//playerTank.Move(MoveDir::Down);
+			break;
+		case VK_LEFT:
+			//playerTank.Move(MoveDir::Left);
+			break;
+		case VK_RIGHT:
+			//playerTank.Move(MoveDir::Right);
+			break;
+		}
+		break;
+
+	case WM_LBUTTONDOWN:
+		clickedMousePosX = LOWORD(lParam);
+		clickedMousePosY = HIWORD(lParam);
+		break;
+	case WM_LBUTTONUP:
+		break;
+	case WM_RBUTTONDOWN:
+		break;
+	case WM_MOUSEMOVE:
+		mousePosX = LOWORD(lParam);
+		mousePosY = HIWORD(lParam);
+		break;
+	}
+	return DefWindowProc(hWnd, iMessage, wParam, lParam);
+}
